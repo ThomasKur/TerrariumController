91eeb03992a4686c4777968110d91f89db376582
Thomas Kurth <thomas@famkurth.ch>
2026-05-25 09:33:25 +0200

    cleanup

--- PiSource/TerrariumController/Services/RelayCommandCoordinatorOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace TerrariumController.Services
-{
-    public class RelayCommandCoordinatorOptions
-    {
-        public TimeSpan ManualOverrideDuration { get; set; } = TimeSpan.FromMinutes(10);
-    }
-}
